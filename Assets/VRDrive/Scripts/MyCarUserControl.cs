976d0b0c0c7e59aced7a78b581609f935091f3e6
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2016-11-04 13:34:17 +0900

    enable to drive using brake and repair the function about accel

--- Assets/VRDrive/Scripts/MyCarUserControl.cs
+++ Assets/VRDrive/Scripts/MyCarUserControl.cs
@@ -21,12 +21,18 @@ namespace UnityStandardAssets.Vehicles.Car
 			if (h == 0) {
 				h = CrossPlatformInputManager.GetAxis("Horizontal");
 			}
-			float v = Input.GetAxis("Accel");
-			v = (v * (-1f) + 1f) * 0.5f;
-			if(v == 0) {
-				v = CrossPlatformInputManager.GetAxis("Vertical");
+			float v = Input.GetAxis("Accel") * (-1f);
+			if (v < 0) {
+				v = CrossPlatformInputManager.GetAxis ("Vertical");
+			} else {
+				v = (v  + 1f) * 0.5f;
 			}
-			float b = CrossPlatformInputManager.GetAxis("Space");
+			float b = Input.GetAxis("Brake") * (-1f);
+			if (b < 0) {
+				b = CrossPlatformInputManager.GetAxis("Space");
+			} else {
+				b = (b  + 1f) * 0.5f;
+			}
 			if(b == 0) {
 				if(v > 0) {
 					m_Car.Move(h, v, v, 0);
