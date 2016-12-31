d1fcc8c8d1ebe697021a80017d6d3161870a9a72
4885rhkA <sun.oon.bow.reverse7@gmail.com>
2016-12-31 17:34:54 +0900

    リアホイールに変更

--- Assets/VRDrive/Scripts/MyCarController.cs
+++ Assets/VRDrive/Scripts/MyCarController.cs
@@ -18,7 +18,7 @@ namespace UnityStandardAssets.Vehicles.Car
 
     public class MyCarController : MonoBehaviour
     {
-        [SerializeField] private CarDriveType m_CarDriveType = CarDriveType.FourWheelDrive;
+		[SerializeField] private CarDriveType m_CarDriveType = CarDriveType.RearWheelDrive;
         [SerializeField] private WheelCollider[] m_WheelColliders = new WheelCollider[4];
         [SerializeField] private GameObject[] m_WheelMeshes = new GameObject[4];
         [SerializeField] private WheelEffects[] m_WheelEffects = new WheelEffects[4];
